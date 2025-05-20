6bebd1c4edfa197fad8d86e88b7f2bfb1fe03584
luizfernandofolchini <147443269+luizfernandofolchini@users.noreply.github.com>
2025-05-20 13:51:40 -0300

    20/05

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 const botoes = document.querySelector(".botao");
+
+for (let i = 0; i < botoes.lenght; i++) {
+    botoes[i].onclick = function () {
+        botoes[i].classlist.add(".ativo");
+    };
+}
