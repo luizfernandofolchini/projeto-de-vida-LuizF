89e623f23435575ea731d3ae345b226370077637
luizfernandofolchini <147443269+luizfernandofolchini@users.noreply.github.com>
2025-06-03 14:28:29 -0300

    3/6/25

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ const textos = document.querySelectorAll(".aba-conteudo");
 for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
 
-for (let j=0; j<botoes.length; j++){
-    botoes[j].classList.remove("ativo");
-    textos[j].classList.remove("ativo");
-}
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
 
         botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
@@ -15,7 +15,7 @@ for (let j=0; j<botoes.length; j++){
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2025-12-19T:00:00");
+const tempoObjetivo1 = new Date("2025-12-19T00:00:00");
 let tempoAtual = new Date();
 
-contadores[0].textContent = tempoObjetivo1-tempoAtual;
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
