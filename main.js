4b190dcca4e565cee89c50f3b31b5f52e3ae0166
luizfernandofolchini <147443269+luizfernandofolchini@users.noreply.github.com>
2025-06-10 14:24:24 -0300

    10/06

--- main.js
+++ main.js
@@ -26,16 +26,29 @@ const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4]
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
-    let tempoFinal = tempoObjetivo-tempoAtual;
-    let segundos = Math.floor(tempoFinal/1000);
-    let minutos = Math.floor(segundos/60);
-    let horas = Math.floor(minutos/60);
-    let dias = Math.floor(horas/24);
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
 
     segundos %= 60;
-    minutos %=60;
-    horas%=24;
+    minutos %= 60;
+    horas %= 24;
 
-    return(dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ");
+    return (dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ");
+}
+
+function atualizaCronometro() {
+    for (let i = 0; i < contadores.length; i++) {
+        contadores[i].textContent = calculaTempo(tempos[i]);
+    }
 
 }
+
+function comecaCronometro() {
+    atualizaCronometro();
+    setInterval(atualizaCronometro, 1000)
+}
+
+comecaCronometro();
