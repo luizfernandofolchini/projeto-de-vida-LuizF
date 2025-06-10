cc91a9eb614cc78e682069a3efc5b1270fddc912
luizfernandofolchini <147443269+luizfernandofolchini@users.noreply.github.com>
2025-06-10 14:15:35 -0300

    10/06

--- main.js
+++ main.js
@@ -16,8 +16,13 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-12-19T00:00:00");
+const tempoObjetivo2 = new Date("2026-12-01T00:00:00");
+const tempoObjetivo3 = new Date("2026-01-01T00:00:00");
+const tempoObjetivo4 = new Date("2026-03-01T00:00:00");
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4]
+
+
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
